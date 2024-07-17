939337d8ca37cd6ff78bd785bfec565c19b69ad0
kausaralishaikh <shaikhkausarali771@gmail.com>
2024-07-17 13:28:33 +0530

    Update lecture_7.py

--- lecture_7.py
+++ lecture_7.py
@@ -1,3 +1,4 @@
+#lecture 7
 f_name=input("enter first name")
 m_name=input("enter middle name")
 l_name=input("enter last name")
@@ -14,3 +15,9 @@ e_email=input("enter your email")
     DOB= input("DDMMYYYY")
     b=DOB[0:4]
     password=a+b
+ #code2
+ L1 = [1,2,3,4]
+ def print_star(L1):
+    for i in L1:
+        print("*" * i)
+ print_star(L1)
