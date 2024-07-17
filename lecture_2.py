54922806bfcbfbed4eb2f856b4ac26381c680982
kausaralishaikh <shaikhkausarali771@gmail.com>
2024-07-17 13:34:56 +0530

    Update lecture_2.py

--- lecture_2.py
+++ lecture_2.py
@@ -6,3 +6,13 @@ e=int(input("subject 5"))
 total_marks = a + b + c + d + e
 percentage = (total_marks / 5)
 print("Final percentage:", percentage)
+if percentage < 35:
+    print("fail")
+elif percentage >= 35 and percentage < 40:
+    print("passed class")
+elif percentage >= 40 and percentage < 60:
+    print("second class")
+elif percentage >= 60 and percentage < 75:
+    print("first class")
+elif percentage >= 75:
+    print("distinction")
