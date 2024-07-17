b33270593a8dc513441fc479ea8101dd8908d9de
kausaralishaikh <shaikhkausarali771@gmail.com>
2024-07-17 13:16:44 +0530

    Create lecture_7.py

---
+++ lecture_7.py
@@ -0,0 +1,16 @@
+f_name=input("enter first name")
+m_name=input("enter middle name")
+l_name=input("enter last name")
+#name="_".join([f_name,m_name,l_name])
+print(" ".join([f_name,m_name,l_name]))
+e_email=input("enter your email") 
+ if email.endswith('''com,.org,.edu,.in''')
+    print("valid")
+  else:
+    print("invalid")
+    fname="kausar"
+    fname-fname.lower()
+    @=fname[0:4]
+    DOB= input("DDMMYYYY")
+    b=DOB[0:4]
+    password=a+b
