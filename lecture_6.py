23f82c8a666cfc8a07960268817cc624523720d1
kausaralishaikh <shaikhkausarali771@gmail.com>
2024-07-17 13:31:04 +0530

    Update lecture_6.py

--- lecture_6.py
+++ lecture_6.py
@@ -1,4 +1,5 @@
 #lecture6
+#code 1
 def add(i,j):
     return i+j
 def sub(i,j):
@@ -24,3 +25,14 @@ def calculator():
         print("error")
 
 print(calculator())
+#code 2
+a=int(input("enter a"))
+b=int(input("enter b"))
+c=int(input("enter c"))
+def maximum(a,b):
+    if a>b:
+        print(a,"is biggest")
+    else:
+        print(b,"is biggest")
+    
+maximum(c,max(b,a))  
